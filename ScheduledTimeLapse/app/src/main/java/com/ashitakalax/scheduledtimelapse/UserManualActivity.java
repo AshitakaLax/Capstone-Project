ff21409ab1870271e2de9056ff7399c4f8b2cb4b
levi Balling <leviballing@gmail.com>
2016-08-09 15:04:53 -0600

    added support for some AsyncTask fetching to get the usermanual from online. if there is a network connection available. I still need to add that to the manifest.

--- ScheduledTimeLapse/app/src/main/java/com/ashitakalax/scheduledtimelapse/UserManualActivity.java
+++ ScheduledTimeLapse/app/src/main/java/com/ashitakalax/scheduledtimelapse/UserManualActivity.java
@@ -8,7 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.widget.TextView;
 
+import com.ashitakalax.scheduledtimelapse.data.FetchUsermanualTask;
+import com.ashitakalax.scheduledtimelapse.utils.NetworkingUtils;
 import com.commonsware.cwac.anddown.AndDown;
+
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -20,30 +24,40 @@ import java.security.PublicKey;
  * Created by lballing on 7/26/2016.
  * this will use AndDown to render markup to  simplify formatting the user manual
  */
-public class UserManualActivity extends AppCompatActivity {
+public class UserManualActivity extends AppCompatActivity implements FetchUsermanualTask.OnUserManualReceiveInterface {
 
     private String userManualUrl = "https://raw.githubusercontent.com/AshitakaLax/Capstone-Project/master/README.md";
-
+    private FetchUsermanualTask mFetchUserManual;
+    private TextView mUserManualTextView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.markdown_layout);
-        TextView userManualTextView = (TextView)findViewById(R.id.user_manual_textview);
+        this.mUserManualTextView = (TextView)findViewById(R.id.user_manual_textview);
 
-        String raw=readRawTextFile(this, R.raw.readme);
+
+        String userManualRaw = "";
+        if(NetworkingUtils.isNetworkAvailable(this))
+        {
+            new FetchUsermanualTask(this).execute(userManualUrl);
+        }
+        else
+        {
+            //todo we need to revert to a previous version of the usermanual already stored on the phone in a key Value Pair
+            userManualRaw = readRawTextFile(this, R.raw.readme);
+        }
 
         AndDown convert = new AndDown();
 
-        String cooked = convert.markdownToHtml(raw);
+        String cooked = convert.markdownToHtml(userManualRaw);
 
         CharSequence charSequence = Html.fromHtml(cooked);
 
-        userManualTextView.setText(charSequence);
     }
 
     /**
-     * This function is here just in case I want to store the user manual updates as a text file
+     * This function is here just in case I want to store the default user manual, if there never is an internet connection
      * @param ctx context
      * @param resId resource Id
      * @return string from the text file
@@ -65,5 +79,14 @@ public class UserManualActivity extends AppCompatActivity {
             return null;
         }
         return text.toString();
+    }
+
+    /**
+     * This will be the converted CharSequence that we need to display on the UI
+     * @param sequence formatted Char Sequence
+     */
+    @Override
+    public void OnUserManualReceived(CharSequence sequence) {
+        mUserManualTextView.setText(sequence);
     }
 }
---
+++ ScheduledTimeLapse/app/src/main/java/com/ashitakalax/scheduledtimelapse/data/FetchUsermanualTask.java
@@ -0,0 +1,122 @@
+package com.ashitakalax.scheduledtimelapse.data;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.text.Html;
+import android.util.Log;
+
+import com.commonsware.cwac.anddown.AndDown;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by lballing on 8/9/2016.
+ * This Class is to fetch the Usermanual from the given URL, and then update what is stored based on that.
+ */
+public class FetchUsermanualTask extends AsyncTask<String, Void, CharSequence> {
+    private final String TAG = this.getClass().getSimpleName();
+
+    public interface OnUserManualReceiveInterface{
+        void OnUserManualReceived(CharSequence sequence);
+    }
+
+    public FetchUsermanualTask(OnUserManualReceiveInterface userManualCallback)
+    {
+        if(userManualCallback == null)
+        {
+            throw new IllegalArgumentException("userManualCallback can't be null");
+        }
+        this.mUserManualCallback = userManualCallback;
+    }
+
+    private OnUserManualReceiveInterface mUserManualCallback;
+
+    /**
+     * This is where we fetch the json String
+     * @param urlTarget to fetch the data
+     * @return the Markdown string
+     */
+    @Override
+    protected CharSequence doInBackground(String... urlTarget) {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String userManualStr = null;
+        if(this.mUserManualCallback == null)
+        {
+            return null;
+        }
+        try {
+            URL url = new URL(urlTarget[0]);
+
+            urlConnection = (HttpURLConnection)url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return "";
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line);
+                buffer.append("\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return "";
+            }
+            userManualStr = buffer.toString();
+        }
+        catch (Exception ex)
+        {
+            Log.e(TAG, ex.getMessage());
+        }finally{
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(TAG, "Error closing stream", e);
+                }
+            }
+        }
+        // convert the raw markdown text into html, then to a character sequence to be displayed
+
+        AndDown convert = new AndDown();
+
+        String cooked = convert.markdownToHtml(userManualStr);
+
+        CharSequence charSequence = Html.fromHtml(cooked);
+
+        return charSequence;
+    }
+
+    /**
+     * The data has come back, and we want to just forward that data on
+     * @param s
+     */
+    @Override
+    protected void onPostExecute(CharSequence s) {
+        super.onPostExecute(s);
+        if(this.mUserManualCallback == null)
+        {
+            return;
+        }
+        this.mUserManualCallback.OnUserManualReceived(s);
+    }
+}
